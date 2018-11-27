e44e5f6201194cc2feeda7e9fe40918870aa81b0
ryryfasch <ryan.fasching@colorado.edu>
2018-11-27 11:34:50 -0700

    more server.js skeleton

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+var SerialPort = require('serialport');
 
 const app = express();
 
@@ -6,12 +7,25 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs')
 app.use(express.static('public'));
 
+var serialPort = new SerialPort("COM11", {
+   baudRate: 9600
+});
+
+var serialPortCallback = function(err) {
+   if(err) {
+       console.log("Error on write: ", err.message);
+   }
+};
+
 app.get('/', function(req, res){
   // res.render('index', {title: 'Search for whatever you want'});
 })
 
 app.post('/', function (req, res) {
-  
+  serialPort.on('open', function() {
+     console.log("Connected\n");
+     serialPort.write(req.body.command, serialPortCallback);
+  });
   // res.render('index0');
 })
 
