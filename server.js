3a5d00f96d16d927ce8c4008042ecb363fd219d4
ryryfasch <ryan.fasching@colorado.edu>
2018-11-28 15:02:56 -0700

    writing to sparki

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.static(__dirname + '/views'));
 //   });
 // });
 
-var command = "";
+
 var serialPort = new SerialPort("COM11", {
    baudRate: 9600
 });
@@ -29,57 +29,28 @@ var serialPortCallback = function(err) {
 };
 
 
-
+serialPort.on('open', function() {
+  console.log("Connected1\n");
+  // serialPort.write('f', serialPortCallback);
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/views/index.html');
   // res.render('index', {title: 'Search for whatever you want'});
-  serialPort.on('open', function() {
-    console.log("Connected1\n");
-    // serialPort.write('f', serialPortCallback);
- });
 })
 
 app.post('/', function (req, res) {
-    console.log("request " + req.body.command);
-    // console.log(util.inspect(req.body.command, {showHidden: false, depth: null}))
-
-    if(req.body.command == "forward"){
-      command = "forward";
-      serialPort.on('open', function() {
-         console.log("Connected2\n");
-         serialPort.write(command, serialPortCallback);
-      });
+    console.log("request " + req.body);
+    // console.log(util.inspect(req.body, {showHidden: false, depth: null}))
+    if(req.body.command == 'forward'){
+      serialPort.write(req.body.command, serialPortCallback);
     }
-    if(req.body.command == "backward"){
-      command = "backward";
-      serialPort.on('open', function() {
-         console.log("Connected2\n");
-         serialPort.write(command, serialPortCallback);
-      });
-    }
-    if(req.body.command == "turn left"){
-      command = "left"; // 90 degree turn
-      serialPort.on('open', function() {
-         console.log("Connected2\n");
-         serialPort.write(command, serialPortCallback);
-      });
-    }
-    if(req.body.command == "turn right"){
-      command = "right"; // 90 degree turn
-      serialPort.on('open', function() {
-         console.log("Connected2\n");
-         serialPort.write(command, serialPortCallback);
-      });
-    }
-    if(req.body.command == "stop"){
-      command == "stop";
-      serialPort.on('open', function() {
-         console.log("Connected2\n");
-         serialPort.write(command, serialPortCallback);
-      });
-    }
+  // serialPort.on('open', function() {
+  //    console.log("Connected2\n");
+  //    serialPort.write(req.body.command, serialPortCallback);
+  // });
   // res.render('index0');
 })
+
+});
 
 app.listen((process.env.PORT || 3000), function () {
   console.log('Example app listening on port 3000!')
